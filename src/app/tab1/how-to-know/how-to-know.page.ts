1e639dd9ff8e8bf6684163ec4108d16e6c726c0d
gustavosalm <gustavosalmeida9821@gmail.com>
2023-04-19 19:25:49 -0300

    how to know page part 2

--- src/app/tab1/how-to-know/how-to-know.page.ts
+++ src/app/tab1/how-to-know/how-to-know.page.ts
@@ -9,10 +9,14 @@ import { Router } from '@angular/router';
 export class HowToKnowPage implements OnInit {
   checkboxes: any = [];
   formContainers: any = [];
-  tooltipCard: any;
-  boxStates: number[] = [-1, -1, -1, -1, -1];
+  containerTop: any;
+  containerFooter: any;
+  fillBar: any;
+  fillBarText: any;
+  boxStates: number[] = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
   boxDenies = false;
   allChecked = 0;
+  currentItem = 0;
   formsPart = 1;
 
   constructor(private router : Router) { }
@@ -20,7 +24,10 @@ export class HowToKnowPage implements OnInit {
   ngOnInit() {
     this.checkboxes = document.getElementsByClassName('listItem');
     this.formContainers.push(document.getElementById('questionsContainer1'), document.getElementById('questionsContainer2'))
-    this.tooltipCard = document.getElementsByClassName('staticTooltipCard');
+    this.containerTop = document.getElementsByClassName('containerTop');
+    this.containerFooter = document.getElementsByClassName('questionsFooter');
+    this.fillBar = document.getElementById('fillBar');
+    this.fillBarText = document.getElementById('fillBarText');
   }
 
   checkingBox(ind: number, box: number) {
@@ -30,11 +37,12 @@ export class HowToKnowPage implements OnInit {
       // item.children[box+1].children[1].checked = true;
       this.boxStates[ind] = -1;
       this.allChecked--;
+      this.refreshCurrentItem();
       return;
     }
     else if(this.boxStates[ind] !== -1) {
       this.boxDenies = true;
-      item.children[this.boxStates[ind]+1].children[1].checked = false;
+      item.children[this.boxStates[ind]+this.formsPart].children[1].checked = false;
       setTimeout(() => {
         this.boxDenies = false;
       }, 100);
@@ -42,25 +50,73 @@ export class HowToKnowPage implements OnInit {
     else {
       this.allChecked++;
     }
+    this.currentItem = ind;
     this.boxStates[ind] = box;
+    this.refreshCurrentItem();
   }
 
   changePart(ind : number){
-    console.log('sss');
-
     if(ind === 1) {
-      this.tooltipCard[0].classList.remove('hidden');
+      this.containerTop[0].classList.remove('hidden');
+      this.containerTop[1].classList.add('hidden');
       this.formContainers[0].classList.remove('contCollapsed');
       this.formContainers[1].classList.add('contCollapsed');
       this.formContainers[0].children[0].scrollTop = 0;
       this.formsPart = ind;
     }
     else if(ind === 2) {
-      this.tooltipCard[0].classList.add('hidden');
+      this.containerTop[0].classList.add('hidden');
+      this.containerTop[1].classList.remove('hidden');
       this.formContainers[0].classList.add('contCollapsed');
       this.formContainers[1].classList.remove('contCollapsed');
       this.formsPart = ind;
+      this.currentItem = 5;
+      this.refreshCurrentItem();
     }
+  }
+
+  refreshCurrentItem(){
+    if(this.currentItem < 5)
+      return;
+    if(this.boxStates[this.currentItem] === -1)
+      this.containerFooter[0].classList.add('disabled');
+    else
+      this.containerFooter[0].classList.remove('disabled');
+
+    this.fillBar.style.width = (this.currentItem - 4)*100/6  + '%';
+    this.fillBarText.innerHTML = (this.currentItem - 4) + ' de 6'
+  }
+
+  nextIndex(){
+    if(this.currentItem + 1 > 10) return;
+
+    if(this.currentItem + 1 === 10) {
+      this.containerFooter[0].children[1].innerHTML = 'Resultado';
+    }
+    else if(this.currentItem === 5){
+      this.containerFooter[0].classList.remove('collapsedLeft');
+    }
+
+    this.checkboxes[this.currentItem].classList.remove('current');
+    this.checkboxes[this.currentItem+1].classList.add('current');
+    this.currentItem++;
+    this.refreshCurrentItem();
+  }
+
+  previousIndex(){
+    if(this.currentItem - 1 < 5) return;
+
+    if(this.currentItem === 10) {
+      this.containerFooter[0].children[1].innerHTML = 'Próximo';
+    }
+    else if(this.currentItem - 1 === 5) {
+      this.containerFooter[0].classList.add('collapsedLeft');
+    }
+
+    this.checkboxes[this.currentItem].classList.remove('current');
+    this.checkboxes[this.currentItem-1].classList.add('current');
+    this.currentItem--;
+    this.refreshCurrentItem();
   }
 
   goBack(){
